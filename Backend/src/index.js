439111241545e12cc6a7b0a96a91a238839474d2
GrazewalSingh <singhgrazewal2@gmail.com>
2022-03-29 20:47:52 +0530

    third push 29-03

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 const express= require("express")
-
+const passport= require("./configs/facebook.oath")
+const path= require("path")
 var cors = require('cors')
 const app=express();
 app.use(express.json())
@@ -7,6 +8,18 @@ app.use(cors())
 module.exports=app
 const  RegisterController=require("./controller/Register.controller");
 const loginController= require("./controller/login.controller")
+// const facebookOauth=require("./configs/facebook.oath")
 app.use("/register",RegisterController)
 app.use("/login",loginController)
- 
+// app.use("/auth/facebook", facebookOauth)
+app.get('/auth/facebook',
+  passport.authenticate('facebook',{scope: 'public_profile,email'}));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' , session:false}),
+  function(req, res) {
+    // Successful authentication, redirect home.
+     
+    res.redirect('/');
+     
+  });
