a5d8685a3ec1e6df7d53f53f3c83da82ff3514d5
GrazewalSingh <singhgrazewal2@gmail.com>
2022-03-30 12:57:14 +0530

    .

--- Backend/src/configs/facebook.oath.js
+++ Backend/src/configs/facebook.oath.js
@@ -1,44 +1,44 @@
-// const express=require("express");
-// const router= express.Router();
-const jwt = require('jsonwebtoken');
- const passport =require("passport")
-require("dotenv").config()
-const { v4: uuidv4 } = require('uuid');
-const FacebookStrategy = require('passport-facebook').Strategy;
-const newToken=(user)=>{
-    return jwt.sign({user},process.env.SECRET_KEY)
-}
+// // const express=require("express");
+// // const router= express.Router();
+// const jwt = require('jsonwebtoken');
+//  const passport =require("passport")
+// require("dotenv").config()
+// const { v4: uuidv4 } = require('uuid');
+// const FacebookStrategy = require('passport-facebook').Strategy;
+// const newToken=(user)=>{
+//     return jwt.sign({user},process.env.SECRET_KEY)
+// }
 
-const User=require("../model/user.model")
+// const User=require("../model/user.model")
  
 
 
-passport.use(new FacebookStrategy({
-    clientID:  552345559531253,
-    clientSecret:"afe8867f7681fe2b93354ff2801ef4a7",
-    callbackURL: "http://localhost:4000/auth/facebook/callback",
-    profileFields: ['id', 'displayName', 'photos', 'email']
-  },
-  async function(accessToken, refreshToken, profile, cb) {
-    // console.log( profile)
+// passport.use(new FacebookStrategy({
+//     clientID:  552345559531253,
+//     clientSecret:"afe8867f7681fe2b93354ff2801ef4a7",
+//     callbackURL: "http://localhost:4000/auth/facebook/callback",
+//     profileFields: ['id', 'displayName', 'photos', 'email']
+//   },
+//   async function(accessToken, refreshToken, profile, cb) {
+//     console.log( profile)
 
-    let user= await User.findOne({email:profile?._json?.email}).lean().exec()
+//     let user= await User.findOne({email:profile?._json?.email}).lean().exec()
    
-   if(!user){
-         user= await User.create({
-            username:profile._json.name,
-            email:profile._json.email,
-            password:uuidv4()
+//    if(!user){
+//          user= await User.create({
+//             username:profile._json.name,
+//             email:profile._json.email,
+//             password:uuidv4()
     
-        })
-    }
+//         })
+//     }
    
-    //   console.log(user)
-    return cb(null,user);
+//       console.log(user)
+//     return cb(null,user);
   
-}
-));
-module.exports= passport
+// }
+// ));
+// module.exports= passport
 
 
 
--- script/slide.js
+++ script/slide.js
@@ -3,7 +3,7 @@ document.getElementById("1dots").style.opacity=1
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
-console.log("jaySanodiya");
+ 
 productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
