9bb605be843ed1714152428574c1541d6346c9d0
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-02 23:41:54 +0530

    Updated Index

--- backend_code/src/index.js
+++ backend_code/src/index.js
@@ -56,7 +56,7 @@ app.get(
     // return res.send({token:res.req.authInfo,user: res.req.user});
     console.log(res.req.user);
     console.log(res.req.authInfo);
-    res.redirect("http://127.0.0.1:5500/index.html");
+    res.redirect("http://127.0.0.1:5501/index.html");
   }
 );
 
